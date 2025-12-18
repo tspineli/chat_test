dd53318fc7e69492e22bb25063ad981d5f9cb637
tspineli <tspineli02@gmail.com>
2025-12-18 13:45:34 -0300

    Update vite.config.ts

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "node:path";
 
 export default defineConfig({
   plugins: [react()],
+  cacheDir: path.resolve(__dirname, ".vite-cache"),
   server: { host: "0.0.0.0", port: 3000 }
 });
